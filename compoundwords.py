aac69b9489481092212a51bd769f6d7fb8524e42
David R <quintopia@gmail.com>
2016-09-06 20:41:53 -0400

    compound word pruner WORKING

--- compoundwords.py
+++ compoundwords.py
@@ -5,12 +5,21 @@ import gzip
 import copy
 import dawg
 from operator import mul
+import time
 
+def memoize(f):
+    """ Memoization decorator for a function taking a single argument """
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret 
+    return memodict().__getitem__
     
 with gzip.open('engmodel1.json.gz', 'rb') as f:
     countdict = json.load(f,object_pairs_hook=sdict)
 ddawg = dawg.CompletionDAWG(countdict.keys())
 
+@memoize
 def tokenize(word):
     #print word
     prefices = ddawg.prefixes(word)
@@ -19,40 +28,41 @@ def tokenize(word):
         l = [[word]]
     for prefix in prefices[:-1]:
         suffix = word[len(prefix):]
-        #import pdb;pdb.set_trace()
         for tokenlist in tokenize(suffix):
-            tokenlist.insert(0,prefix)
-            if tokenlist not in l:
-                l.append(tokenlist)
+            newlist = [prefix]
+            newlist.extend(tokenlist) #tokenlist is an element of a memoized return value, so we must not modify it. CAN'T USE tokenlist.insert!
+            if newlist not in l:
+                l.append(newlist)
     return l
 
-print tokenize(u"wholesome")
-"""total = countdict[countdict.last_key()]
+total = sum(countdict.values())
+print total
+wtd = {}
 
 sys.stdout.write("Words loaded. Processing");sys.stdout.flush()
 for i,word in enumerate(countdict.keys()):
     currelfreq = float(countdict[word])/total
     maxsofar = currelfreq
     bestsplit = word
-    toks = tokenize(word) 
-    if toks:
-        print toks[0]
-    for tokenization in tokenize(word):
-        print tokenization
+    toks = tokenize(word)
+    for tokenization in toks[1:]:
         if tokenization:
-            score = float(reduce(mul, [countdict[j] for j in tokenization]))/total**len(tokenization)
+            score = reduce(mul, [countdict[j]/float(total) for j in tokenization])
             if score>maxsofar:
                 maxsofar = score
                 bestsplit = tokenization
     if maxsofar > currelfreq:
-        val = countdict[word]
-        del countdict[word]
-        sys.stdout.write("\nDeleted "+word);sys.stdout.flush()
-        for token in bestsplit:
-            countdict[token] += val
+        sys.stdout.write("\nGoing to delete "+word);sys.stdout.flush()
+        wtd[word] = bestsplit
     if not i%500:
         sys.stdout.write(".");sys.stdout.flush()
-
+print "done!"
+sys.stdout.write("Deleting uncommons");sys.stdout.flush()
+for word,split in wtd.items():
+    val = countdict[word]
+    del countdict[word]
+    for token in split:
+        countdict[token] += val
 print "done!"
 sys.stdout.write("Saving new model...");sys.stdout.flush()
 with gzip.open('engmodel3.json.gz', 'wb') as f:
